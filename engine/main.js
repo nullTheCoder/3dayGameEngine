aa99bf3cc9704ee77159de7032e46227aedf1c5d
Степан гайдукевич <stepangajdukevic@Stepans-MacBook-Pro.local>
2020-07-30 13:22:18 +0300

    no

--- engine/main.js
+++ engine/main.js
@@ -1,6 +1,14 @@
 
 let updateFuncs = []
 
+let width = 0
+
+let height = 0
+
+let background_symbols = []
+
+let background_render = ""
+
 function start() {
     document.getElementById("mainText").textContent = "Made using \n 3-day game engine \n which was made in 3 days"
 
@@ -11,6 +19,7 @@ function start() {
 function addUpdates(func)
 {
     updateFuncs.push(func)
+    render()
 }
 
 
@@ -20,4 +29,25 @@ function everyUpdate() {
     {
         updateFuncs[i]()
     }
+}
+
+
+function setScreen(width_, height_) {
+    width = width_
+    height = height_
+}
+
+function displayBackground(list_of_strings)
+{
+    background_symbols = list_of_strings
+    console.log("test");
+}
+
+function render() {
+    background_render = ""
+    for (i = 0; i < background_symbols.length; i++) {
+        background_render = background_render + background_symbols[i] + "/n"
+    }
+
+    document.getElementById("mainText").textContent = background_render
 }
--- game/start.js
+++ game/start.js
@@ -2,9 +2,12 @@ let i = 0
 
 function startGame() {
     addUpdates(update)   
-
+    setScreen(3, 2)
 }
 
 function update() {
-
+    displayBackground([
+        "111",
+        "101"
+    ])
 }
