18adc72b1b076dc0560cf0f72eb0aa77db3a366f
Степан гайдукевич <stepangajdukevic@Stepans-MacBook-Pro.local>
2020-07-30 13:02:12 +0300

    heyyyy

--- engine/main.js
+++ engine/main.js
@@ -1,15 +1,23 @@
-let i = 0
 
+let updateFuncs = []
 
 function start() {
-    document.getElementById("mainText").textContent = "Made using \n     3-day game engine \n       which was made in 3 days"
+    document.getElementById("mainText").textContent = "Made using \n 3-day game engine \n which was made in 3 days"
 
     setInterval(everyUpdate, 20)
 
 }
 
+function addUpdates(func)
+{
+    updateFuncs.push(func)
+}
+
+
 
 function everyUpdate() {
-    i++
-    document.getElementById("mainText").textContent = parseInt(i / 50)
+    for (i = 0; i < updateFuncs.length; i++)
+    {
+        updateFuncs[i]()
+    }
 }
---
+++ game/start.js
@@ -0,0 +1,10 @@
+let i = 0
+
+function startGame() {
+    addUpdates(update)   
+
+}
+
+function update() {
+
+}
